40ce108c3e434716a929bcc85eeecc268c8449d4
prakash-2002 <lovelyprakash650@gmail.com>
2024-08-31 00:01:54 +0530

    file updated

--- MUI/src/components/Navbar.jsx
+++ MUI/src/components/Navbar.jsx
@@ -1,19 +1,102 @@
-import { AppBar, Box, styled, Toolbar } from "@mui/material";
-import React from "react";
-
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import profilePic from "../assets/profile.jpg";
+import { Pets, Mail, Notifications } from "@mui/icons-material";
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
-  justifycontent: "space-between",
+  justifyContent: "space-between",
 });
 
-function Navbar() {
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
+const Userbox = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <Box>
-      <AppBar position="sticky">
-        <StyledToolbar>Navbar</StyledToolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Prakash Dev
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            onClick={(e) => setOpen(true)}
+            sx={{ width: "30px", height: "30px" }}
+            src={profilePic}
+          />
+        </Icons>
+        <Userbox>
+          <Avatar
+            onClick={(e) => setOpen(true)}
+            sx={{ width: "30px", height: "30px" }}
+            src={profilePic}
+          />
+          <Typography variant="span">Prakash</Typography>
+        </Userbox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>Logout</MenuItem>
+        <MenuItem>My account</MenuItem>
+      </Menu>
+    </AppBar>
   );
-}
+};
 
 export default Navbar;
