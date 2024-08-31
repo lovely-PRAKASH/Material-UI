9215edef906342c01a4ffe59c35a0e551ee1d28c
prakash-2002 <lovelyprakash650@gmail.com>
2024-08-31 23:28:18 +0530

    31st aug update

--- MUI/src/App.jsx
+++ MUI/src/App.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import RightBar from "./components/RightBar";
 import { Box, Stack } from "@mui/material";
 import Navbar from "./components/Navbar";
-import "./App.css"
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./App.css";
 import Add from "./components/Add";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <Box>
-      <Navbar/>
-      <Stack direction= "row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <RightBar />
-      </Stack>
-      <Add/>
-    </Box>
+    <ThemeProvider darkTheme={darkTheme}>
+      <Box>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <RightBar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
